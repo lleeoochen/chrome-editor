ba7edf82c51f4808fc2c21401fd95014f84cec07
Wei Tung Chen <lleeoochen@gmail.com>
2018-04-03 13:50:40 -0700

    Added address bar icon to open extension on click

--- js/background.js
+++ js/background.js
@@ -2,7 +2,21 @@
 //Author: Wei Tung Chen
 //Date: April 2, 2018
 
+
+//Onclick response to address bar icon to open chrome editor
+chrome.browserAction.onClicked.addListener(function(tab) {
+	openEditor();
+});
+
+
+//Response to chrome-editor.js to open chrome editor
 chrome.runtime.onMessage.addListener(function(request, sender, callback) {
+	openEditor();
+});
+
+
+//Open chrome editor extension
+function openEditor() {
 	var editor_address = chrome.extension.getURL("chrome-editor.html");
 	chrome.tabs.create({url: editor_address});
-});
+}
